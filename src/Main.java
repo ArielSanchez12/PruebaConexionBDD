1d2aaffbf75273d7a8e4b959a04c69f879792607
ArielSanchez12 <riveraariel433@gmail.com>
2024-12-20 11:41:11 -0500

    realizar el deber

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        //descargar la libreria para la conexion a la bdd:
+        //link: https://dev.mysql.com/downloads/connector/j/
+        //Deber para las vacaciones:
+        /*Realizar un login con interfaz grafica que se conecte a la  bdd de Clever Cloud para verificar que las credenciales son correctas
+        Las credenciales son Usuario (Pepito), Nombre de usuario (PeCausa), Contraseña (69master)
+         */
+    }
+}
